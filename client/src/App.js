215f004fa2bbd732c8b68498df7643f18df752cf
Jamal Meizongo <mrmeizongo@thesleepingwalrus.com>
2022-08-08 03:52:57 -0400

    Changed state management to utilize usereducer hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,117 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import Axios from "axios";
 
+let initState = {
+	registerUsername: "",
+	registerPassword: "",
+	loginUsername: "",
+	loginPassword: "",
+	data: null,
+};
+
+function reduce(state, { type, payload }) {
+	switch (type) {
+		case "registerUsername":
+			return { ...state, registerUsername: payload };
+		case "registerPassword":
+			return { ...state, registerPassword: payload };
+		case "loginUsername":
+			return { ...state, loginUsername: payload };
+		case "loginPassword":
+			return { ...state, loginPassword: payload };
+		case "data":
+			return { ...state, data: payload };
+		default:
+			return initState;
+	}
+}
+
 function App() {
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginUsername, setLoginUsername] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-  const [data, setData] = useState(null);
-  const register = () => {
-    Axios({
-      method: "POST",
-      data: {
-        username: registerUsername,
-        password: registerPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost:4000/register",
-    }).then((res) => console.log(res));
-  };
-  const login = () => {
-    Axios({
-      method: "POST",
-      data: {
-        username: loginUsername,
-        password: loginPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost:4000/login",
-    }).then((res) => console.log(res));
-  };
-  const getUser = () => {
-    Axios({
-      method: "GET",
-      withCredentials: true,
-      url: "http://localhost:4000/user",
-    }).then((res) => {
-      setData(res.data);
-      console.log(res.data);
-    });
-  };
-  return (
-    <div className="App">
-      <div>
-        <h1>Register</h1>
-        <input
-          placeholder="username"
-          onChange={(e) => setRegisterUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setRegisterPassword(e.target.value)}
-        />
-        <button onClick={register}>Submit</button>
-      </div>
+	const [state, dispatch] = useReducer(reduce, initState);
+	const {
+		registerUsername,
+		registerPassword,
+		loginUsername,
+		loginPassword,
+		data,
+	} = state;
 
-      <div>
-        <h1>Login</h1>
-        <input
-          placeholder="username"
-          onChange={(e) => setLoginUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setLoginPassword(e.target.value)}
-        />
-        <button onClick={login}>Submit</button>
-      </div>
+	const register = () => {
+		Axios({
+			method: "POST",
+			data: {
+				username: registerUsername,
+				password: registerPassword,
+			},
+			withCredentials: true,
+			url: "http://localhost:4000/register",
+		}).then((res) => console.log(res));
+	};
+	const login = () => {
+		Axios({
+			method: "POST",
+			data: {
+				username: loginUsername,
+				password: loginPassword,
+			},
+			withCredentials: true,
+			url: "http://localhost:4000/login",
+		}).then((res) => console.log(res));
+	};
+	const getUser = () => {
+		Axios({
+			method: "GET",
+			withCredentials: true,
+			url: "http://localhost:4000/user",
+		}).then((res) => {
+			dispatch({ type: "data", payload: res.data });
+			console.log(res.data);
+		});
+	};
+	return (
+		<div className="App">
+			<div>
+				<h1>Register</h1>
+				<input
+					placeholder="username"
+					onChange={(e) =>
+						dispatch({ type: "registerUsername", payload: e.target.value })
+					}
+				/>
+				<input
+					placeholder="password"
+					onChange={(e) =>
+						dispatch({ type: "registerPassword", payload: e.target.value })
+					}
+				/>
+				<button onClick={register}>Submit</button>
+			</div>
 
-      <div>
-        <h1>Get User</h1>
-        <button onClick={getUser}>Submit</button>
-        {data ? <h1>Welcome Back {data.username}</h1> : null}
-      </div>
-    </div>
-  );
+			<div>
+				<h1>Login</h1>
+				<input
+					placeholder="username"
+					onChange={(e) =>
+						dispatch({ type: "loginUsername", payload: e.target.value })
+					}
+				/>
+				<input
+					placeholder="password"
+					onChange={(e) =>
+						dispatch({ type: "loginPassword", payload: e.target.value })
+					}
+				/>
+				<button onClick={login}>Submit</button>
+			</div>
+
+			<div>
+				<h1>Get User</h1>
+				<button onClick={getUser}>Submit</button>
+				{data ? <h1>Welcome Back {data.username}</h1> : null}
+			</div>
+		</div>
+	);
 }
 
 export default App;
